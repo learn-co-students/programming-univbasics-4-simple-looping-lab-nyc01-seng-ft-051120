efad73431fb4a7de8431c34c1d59212d4f109fd4
Fnu Tsering Norbu <thetseringnorbu@gmail.com>
2020-04-22 17:13:39 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,20 @@
 # Write your methods here
+def loop_message_five_times(message)
+  5.times puts message
+end
+def loop_message_n_times(message, n)
+i=0
+  while n>i
+  puts message
+  i +=1
+end
+end
+def output_array(message, n)
+  i=0
+  array=[]
+  until i==n
+    array[i]=message
+    i +=1
+  end
+  puts array
+end
