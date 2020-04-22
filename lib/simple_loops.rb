a98110ec0d78a8f2c2d5ad185704a693ece1d227
masterful-array-3850 <crgammage@gmail.com>
2020-04-22 15:43:36 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -24,9 +24,11 @@ def output_array(array)
 end
 
 def return_string_array(array)
-  counter = 0
-  while counter < array.length
-    array[counter].to_s
+  count = 0
+  new_array = []
+  while count < array.length do
+    new_array.push(array[count].to_s) # OR new_array << array[count].to_s
+    count += 1
   end
-  return array
+  new_array
 end
