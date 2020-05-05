7254435165a3930765a694e40ef292dc38e71a14
Chase <chaserabenn@gmail.com>
2020-05-05 00:37:03 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,25 @@
 # Write your methods here
+#def loop_message_five_times(message)
+#  5.times { puts message }
+#end
+#
+def loop_message_five_times(message)
+  counter = 0
+
+  while counter <= 5
+    puts message
+    counter += 1
+  end
+end
+
+def loop_message_n_times(message, n)
+  n.times { puts message }
+end
+
+def output_array(array)
+  array.each { |i| puts i }
+end
+
+def return_string_array(array)
+  array.map { |el| el.to_s }
+end
